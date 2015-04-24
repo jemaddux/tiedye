72e7beb5953e06b08eca0996bb90adf7093e3ad2
John Maddux <jemaddux@gmail.com>
2015-04-24 19:42:51 -0400

    Code climate coverage hook added to travis ci

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,8 @@
 $:.unshift('.')
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require File.expand_path("../../lib/tiedye.rb", __FILE__)
